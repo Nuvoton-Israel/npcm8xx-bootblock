c0ef3ffe97300e0170484f731029ff89bb3c97fe
Tali Perry <tali.perry1@gmail.com>
2024-05-21 14:37:45 +0300

    Bootblock 0.4.7     https://github.com/Nuvoton-Israel/npcm8xx-bootblock/releases/tag/A35_BootBlock_0.4.7     * Fix DDP\SDP type print.     * Cleanup code for upstream.     * Fix print of reset type for TIP reset case.     * Bug fix: when using dlls_trim_clk override from header option INCR bit value is set to bit 7 instead of 6. Fixed to 6.     * Add mode non-ECC ranges (8 total).     * Fix build on Linux (change Apps folder to apps).     * Upgrade compiler and compile by default with dwarf-3 (allow debugging with Lauterbach, for GDB switch to -ggdb).     * Compile optimization for speed.     * Fix Coverity issues.     * Cleanup makefile.     * Add bit (over scratchpad bits): INTCR2.HOST_INIT  (bit 11). This bit indicates host is initialized by bootblock. After host is set bit is set to prevent re-init.     * Bug fix: cntfrq_el0 was set back to 25000000 after warm boot, regardless of CPU frequency.
    signed-off-by:tali.perry@nuvoton.com

--- Sources/build.sh
+++ Sources/build.sh
@@ -7,16 +7,10 @@
 #   build.sh
 #   Shell script for building code on Linux
 
-# Usage:
-# CC_PATH=xxx build.sh [ENCLAVE]
-# ex: CC_PATH=/home/brian/bin/arm-gnu-toolchain build.sh tip
-set -e
-
 TARGET="arbel"
-BOOTBLOCK="arbel_a35_bootblock"
 
 # ENCLAVE can be tip \ no_tip.  no_tip shoud be used for users who have an external RoT.
-ENCLAVE=${1:-"no_tip"}
+ENCLAVE="$1"
 
 find ./ -type f -name "*.o" -exec rm -vf {} +
 find ./ -type f -name "*.o.d" -exec rm -vf {} +
@@ -30,11 +24,7 @@ if [ -n "$ENCLAVE" ]; then
     DELIV_DIR="./Images/$ENCLAVE"
     mkdir -p "$DELIV_DIR"
 fi
-# now we only support tip or no_tip
-if [ "$ENCLAVE" != "tip" ]; then
-    BOOTBLOCK="${BOOTBLOCK}_no_tip"
-    ENCLAVE="no_tip"
-fi
+
 
 
 echo "========================================"
@@ -43,12 +33,12 @@ echo "========================================"
 
 # Preserve the original PATH
 PATH_BACKUP="$PATH"
-CROSS_COMPILER_PATH=${CC_PATH:-"/work_sw/tperry/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf"}
-CROSS_COMPILER_BIN="${CROSS_COMPILER_PATH}/bin"
-CROSS_COMPILER_LIB="${CROSS_COMPILER_PATH}/lib"
+
+CROSS_COMPILER_PATH="/work_sw/tperry/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin"
+CROSS_COMPILER_LIB="/work_sw/tperry/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/lib"
 MAKE_PATH="/usr/bin/"
 
-if [ ! -d "$CROSS_COMPILER_BIN" ] || [ ! -d "$MAKE_PATH" ]; then
+if [ ! -d "$CROSS_COMPILER_PATH" ] || [ ! -d "$MAKE_PATH" ]; then
     echo "Required tools not found. Make sure the paths are correct."
     exit 1
 fi
@@ -58,18 +48,22 @@ echo "=== Using GNU tools locally ==="
 echo "=============================="
 
 # Prepend paths to the PATH environment variable
-export PATH="$CROSS_COMPILER_BIN:$CROSS_COMPILER_LIB:$MAKE_PATH:$PATH"
+export PATH="$CROSS_COMPILER_PATH:$CROSS_COMPILER_LIB:$MAKE_PATH:$PATH"
 
 echo "========================================"
 echo "= Building Arbel $TARGET $ENCLAVE code"
 echo "========================================"
 
-make "$BOOTBLOCK" ENCLAVE="$ENCLAVE" CROSS_COMPILE="${CROSS_COMPILER_BIN}/aarch64-none-elf-"
+if [ -z "$1" ]; then
+    make arbel_a35_bootblock_no_tip  ENCLAVE="$ENCLAVE" CROSS_COMPILE="$CROSS_COMPILER_PATH" || exit 1
+else
+    make arbel_a35_bootblock  ENCLAVE="$ENCLAVE" CROSS_COMPILE="$CROSS_COMPILER_PATH" || exit 1
+fi
 
 echo "Compile done"
 
 # Make sure outputs are ready
-if [ ! -e "${DELIV_DIR}/${BOOTBLOCK}.bin" ]; then
+if [ ! -e "$DELIV_DIR/Arbel_a35_bootblock.bin" ]; then
     exit 1
 fi
 
